d87b83847f40bab3aac34f26c158049eb71543ed
long14xtnd <longdoan14xtnd@gmail.com>
2024-04-23 17:39:50 +0700

    loader css scss

--- NodeVaNPM/src/main.js
+++ NodeVaNPM/src/main.js
@@ -1,2 +1,4 @@
 import sum from './untils'
+import './styles/style.css'
+import './styles/style.scss'
 console.log(sum(10,4));
--- NodeVaNPM/webpack.config.js
+++ NodeVaNPM/webpack.config.js
@@ -1,11 +1,27 @@
-const path =require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode:'development',
-    entry:{
-        app:path.resolve('src/main.js'),
-    },
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
-    }
-}
+  mode: "production",
+  entry: {
+    app: path.resolve("src/main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "./src/template.html",
+    }),
+  ],
+};
